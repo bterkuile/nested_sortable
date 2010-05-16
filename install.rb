07833a49afaac73b5903583cf77e36cc4f814c64
Benjamin ter Kuile <bterkuile@gmail.com>
2010-05-16 15:06:22 +0200

    Add messages to the install.rb file

--- install.rb
+++ install.rb
@@ -1,5 +1,8 @@
 # Install hook code here
 require 'fileutils'
 include FileUtils
+puts "Copying javascript files..."
 cp_r File.join(__FILE__, 'javascripts'), File.join(Rails.root, 'public', 'javascripts')
+puts "Copying stylesheet.."
 cp_r File.join(__FILE__, 'stylesheets'), File.join(Rails.root, 'public', 'stylesheets')
+puts "Nested sortable files are installed"
