4135f3d663071a4447f15a89ff88495e0a2c4e4a
Benjamin ter Kuile <bterkuile@gmail.com>
2010-05-16 19:18:52 +0200

    recursive copy now done, well not yet right, but it should work

--- install.rb
+++ install.rb
@@ -1,5 +1,7 @@
 # Install hook code here
 require 'fileutils'
 include FileUtils
-cp_r File.join(File.dirname(__FILE__), 'javascripts'), File.join(Rails.root, 'public', 'javascripts')
-cp_r File.join(File.dirname(__FILE__), 'stylesheets'), File.join(Rails.root, 'public', 'stylesheets')
+puts "Copying javascript files..."
+cp_r Dir[File.join(File.dirname(__FILE__), 'javascripts', "*")], File.join(Rails.root, 'public', 'javascripts')
+puts "Copying stylesheet"
+cp_r Dir[File.join(File.dirname(__FILE__), 'stylesheets', "*")], File.join(Rails.root, 'public', 'stylesheets')
